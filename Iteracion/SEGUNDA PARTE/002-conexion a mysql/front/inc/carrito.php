57ee913ec3588092abddb0d396fe64a0a06dd756
Piero Olivares <piero7ov@gmail.com>
2026-02-15 02:42:27 +0100

    Create carrito.php

---
+++ Iteracion/SEGUNDA PARTE/002-conexion a mysql/front/inc/carrito.php
@@ -0,0 +1,33 @@
+ <!-- CARRITO -->
+          <section class="finalizacion">
+            <table>
+              <thead>
+                <tr>
+                  <th>Campo</th>
+                  <th>Hora</th>
+                  <th>Duración</th>
+                  <th>Precio</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Nombre del campo</td>
+                  <td>10:00</td>
+                  <td>2 horas</td>
+                  <td>140€</td>
+                </tr>
+                <tr>
+                  <td>Nombre del campo</td>
+                  <td>18:00</td>
+                  <td>1 hora</td>
+                  <td>70€</td>
+                </tr>
+                <tr>
+                  <td colspan="3">Total</td>
+                  <td>210€</td>
+                </tr>
+              </tbody>
+            </table>
+
+            <a href="?operacion=finalizacion">✅ Confirmar reserva</a>
+          </section>
