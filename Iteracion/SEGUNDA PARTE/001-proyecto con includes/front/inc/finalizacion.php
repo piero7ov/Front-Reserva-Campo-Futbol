e1e9ff212a8791dc41841d6ab4fd3525c5a0549a
Piero Olivares <piero7ov@gmail.com>
2026-02-15 02:39:50 +0100

    Create finalizacion.php

---
+++ Iteracion/SEGUNDA PARTE/001-proyecto con includes/front/inc/finalizacion.php
@@ -0,0 +1,6 @@
+          <!-- FINALIZACIÓN -->
+          <section class="finalizacion">
+            <h3>Reserva finalizada</h3>
+            <p>Muchas gracias por tu reserva</p>
+            <a href="?">⬅ Volver al catálogo</a>
+          </section>
